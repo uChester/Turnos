bc16bc6f15af916f3e14f257032c83f5e452b403
uChester <hiden3414@gmail.com>
2026-09-10 15:39:42 -0300

    cambios de adaptacion b.d

--- src/superbase.js
+++ src/superbase.js
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
-// Leemos las variables de entorno que configuraste en Vercel y localmente
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+// URL fallback por si Vercel no inyecta la variable
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL || 'https://tolqkcsbtnkzudwtdypr.supabase.co';
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRvbHFrY3NidG5renVkd3RkeXByIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODkwMDYxOTYsImV4cCI6MjEwNDU4MjE5Nn0.BRtB_SnocB5iyPu8PDo20k_g-utbittPdxU3KCsjUcY';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
